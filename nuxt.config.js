1242ddb79251c14d418b2f31f25991eb74eceb38
Satyam Singh <129391643+skylinemusiccds@users.noreply.github.com>
2024-05-25 15:25:59 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,47 +3,6 @@ export default {
     rollupOptions: {
       external: ['vuex']
     }
-  },
-  head: {
-    meta: [
-      {
-        charset: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
-      },
-    ],
-    link: [
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Lora',
-      },
-    ],
-  },
-  css: [
-    'normalize.css',
-    '~/assets/sass/base.sass',
-  ],
-  loading: '~/components/the-loader.vue',
-  plugins: [
-    '~/plugins/font-awesome-icon',
-  ],
-  generate: {
-    fallback: true,
-    routes: [
-      '/',
-    ],
-  },
-}
-
-
-
-/*export default {
-  build: {
-    rollupOptions: {
-      external: ['vuex']
-    }
   }
 },
 module.exports = {
@@ -79,4 +38,3 @@ module.exports = {
     ],
   },
 }
-*/
